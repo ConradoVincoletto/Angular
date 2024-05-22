732ec89449a185d2f720ec4fcc24572def52cba0
Conrado Vincoletto <conrado.vincoletto@gmail.com>
2024-05-22 19:49:56 -0300

    Inclusão do ngfor no componente para chamar o array que foi feito noTS

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-eventos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './eventos.component.html',
   styleUrl: './eventos.component.scss'
 })
 export class EventosComponent {
+
+  public eventos: any =
+  [
+    {
+      Tema: 'Angular',
+      Local: 'BH'
+    },
+    {
+      Tema: 'C# DOTNET',
+      Local: 'São Paulo'
+    },
+    {
+      Tema: 'Udemy Angular',
+      Local: 'Rio de Janeiro'
+    }
+  ];
 
 }

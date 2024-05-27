f14a93369b263990521b38aa5d93d1c0e700d74e
Conrado Vincoletto <conrado.vincoletto@gmail.com>
2024-05-27 18:39:43 -0300

    correção do bug devido ao bloqueio do cors

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,29 +1,46 @@
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { subscribe } from 'diagnostics_channel';
+import { response } from 'express';
 
 @Component({
   selector: 'app-eventos',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './eventos.component.html',
   styleUrl: './eventos.component.scss'
 })
 export class EventosComponent {
 
-  public eventos: any =
-  [
-    {
-      Tema: 'Angular',
-      Local: 'BH'
-    },
-    {
-      Tema: 'C# DOTNET',
-      Local: 'São Paulo'
-    },
-    {
-      Tema: 'Udemy Angular',
-      Local: 'Rio de Janeiro'
-    }
-  ];
+  public eventos: any;
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void
+  {
+      this.GetEventos();
+  }
+
+  public GetEventos(): void {
+    this.http.get('https://localhost:5001/api/eventos').subscribe(
+      response => this.eventos = response,
+      error => console.log(error));
+
+    this.eventos =
+    [
+      {
+        Tema: 'Angular',
+        Local: 'BH'
+      },
+      {
+        Tema: 'C# DOTNET',
+        Local: 'São Paulo'
+      },
+      {
+        Tema: 'Udemy Angular',
+        Local: 'Rio de Janeiro'
+      }
+    ]}
 
 }

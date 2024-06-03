8156c37130aff0733468b9bbb27d795ea14db42b
Conrado Vincoletto <conrado.vincoletto@gmail.com>
2024-06-03 18:20:18 -0300

    Instalamos o fortawasome

--- Front/ProEventos-App/src/app/app.component.ts
+++ Front/ProEventos-App/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EventosComponent } from "./eventos/eventos.component";
 import { PalestrantesComponent } from "./palestrantes/palestrantes.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, EventosComponent, PalestrantesComponent]
+    imports: [RouterOutlet, EventosComponent, PalestrantesComponent, BrowserAnimationsModule]
 })
 export class AppComponent {
   title = 'ProEventos-App';
